244317608d7000104816e901546a6147bbbe0292
Lucas Rodrigues <rodrigues14set@hotmail.com>
2021-02-17 17:24:01 -0300

    Add exercises

---
+++ curso_programacao/src/exercicios/ExercicioEstruturaFor01.java
@@ -0,0 +1,18 @@
+package exercicios;
+
+import java.util.Scanner;
+
+public class ExercicioEstruturaFor01 {
+
+	public static void main(String[] args) {
+		/* Fazer um programa para ler um número N. Depois leia N pares de números e mostre a divisão do primeiro pelo
+		segundo. Se o denominador for igual a zero, mostrar a mensagem "divisao impossivel". */
+		
+		Scanner sc = new Scanner(System.in);
+		
+		
+		
+		
+		sc.close();
+	}
+}
